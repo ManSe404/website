25b7a81c862694e304f145fcd0da3f517baab97b
steseman <stefanseman@publicissapient.com>
2024-02-21 20:29:19 +0100

    changes to curriculum link removed index.html and supporting css

--- bin/run.py
+++ bin/run.py
@@ -2,5 +2,4 @@ from src.application.main import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
